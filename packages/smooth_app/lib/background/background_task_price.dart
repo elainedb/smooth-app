f00877b235f73648acf36e9fa6759d6cf7634cab
elaine <elainedb.dev@gmail.com>
2026-03-15 22:26:07 +0100

    “gemini”

--- packages/smooth_app/lib/background/background_task_price.dart
+++ packages/smooth_app/lib/background/background_task_price.dart
@@ -28,6 +28,7 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
     required this.labels,
     required this.pricePers,
     required this.pricesAreDiscounted,
+    required this.discountTypes,
     required this.prices,
     required this.pricesWithoutDiscount,
   });
@@ -51,6 +52,12 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
       pricesAreDiscounted = json.containsKey(_jsonTagIsDiscounted)
           ? <bool>[json[_jsonTagIsDiscounted] as bool]
           : _fromJsonListBool(json[_jsonTagAreDiscounted])!,
+      discountTypes = _normalizeDiscountTypes(
+        _fromJsonListString(json[_jsonTagDiscountTypes]),
+        json.containsKey(_jsonTagBarcode)
+            ? 1
+            : (json[_jsonTagBarcodes] as List<dynamic>).length,
+      ),
       prices = json.containsKey(_jsonTagPrice)
           ? <double>[json[_jsonTagPrice] as double]
           : fromJsonListDouble(json[_jsonTagPrices])!,
@@ -69,6 +76,7 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
   static const String _jsonTagLabels = 'labels';
   static const String _jsonTagPricePers = 'pricePers';
   static const String _jsonTagAreDiscounted = 'areDiscounted';
+  static const String _jsonTagDiscountTypes = 'discountTypes';
   static const String _jsonTagPrices = 'prices';
   static const String _jsonTagPricesWithoutDiscount = 'pricesWithoutDiscount';
   @Deprecated('Use [_jsonTagBarcodes] instead')
@@ -79,6 +87,19 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
   static const String _jsonTagPrice = 'price';
   @Deprecated('Use [_jsonTagPricesWithoutDiscount] instead')
   static const String _jsonTagPriceWithoutDiscount = 'priceWithoutDiscount';
+
+  static List<String> _normalizeDiscountTypes(
+    final List<String>? input,
+    final int expectedLength,
+  ) {
+    final List<String> result = List<String>.filled(expectedLength, '');
+    if (input != null) {
+      for (int i = 0; i < input.length && i < expectedLength; i++) {
+        result[i] = input[i];
+      }
+    }
+    return result;
+  }
 
   static List<double>? fromJsonListDouble(final List<dynamic>? input) {
     if (input == null) {
@@ -155,6 +176,7 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
   final List<List<String>> labels;
   final List<String> pricePers;
   final List<bool> pricesAreDiscounted;
+  final List<String> discountTypes;
   final List<double> prices;
   final List<double?> pricesWithoutDiscount;
 
@@ -171,6 +193,7 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
     result[_jsonTagLabels] = labels;
     result[_jsonTagPricePers] = pricePers;
     result[_jsonTagAreDiscounted] = pricesAreDiscounted;
+    result[_jsonTagDiscountTypes] = discountTypes;
     result[_jsonTagPrices] = prices;
     result[_jsonTagPricesWithoutDiscount] = pricesWithoutDiscount;
     return result;
@@ -237,6 +260,7 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
           labels: <List<String>>[labels[i]],
           pricePers: <String>[pricePers[i]],
           pricesAreDiscounted: <bool>[pricesAreDiscounted[i]],
+          discountTypes: <String>[discountTypes[i]],
           prices: <double>[prices[i]],
           pricesWithoutDiscount: <double?>[pricesWithoutDiscount[i]],
         );
@@ -274,6 +298,9 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
         ..pricePer = isProduct ? null : PricePer.fromOffTag(pricePers[i])
         ..type = isProduct ? PriceType.product : PriceType.category
         ..priceIsDiscounted = priceIsDiscounted
+        ..discountType = priceIsDiscounted
+            ? DiscountType.fromOffTag(discountTypes[i])
+            : null
         ..price = _fixPriceDecimals(price)
         ..priceWithoutDiscount = priceWithoutDiscount == null
             ? null
---
+++ packages/smooth_app/lib/pages/prices/price_discount_type_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:smooth_app/generic_lib/design_constants.dart';
+import 'package:smooth_app/pages/prices/discount_type_extension.dart';
+import 'package:smooth_app/widgets/smooth_dropdown.dart';
+
+/// Dropdown to select a [DiscountType].
+class PriceDiscountTypeDropdown extends StatelessWidget {
+  const PriceDiscountTypeDropdown({
+    required this.value,
+    required this.onChanged,
+    super.key,
+  });
+
+  final DiscountType? value;
+  final ValueChanged<DiscountType?> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    final AppLocalizations appLocalizations = AppLocalizations.of(context);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: SMALL_SPACE),
+      child: SmoothDropdownButton<DiscountType?>(
+        value: value,
+        isExpanded: true,
+        items: <DropdownMenuItem<DiscountType?>>[
+          DropdownMenuItem<DiscountType?>(
+            value: null,
+            child: Text(appLocalizations.prices_discount_type),
+          ),
+          ...DiscountType.values.map(
+            (DiscountType discountType) => DropdownMenuItem<DiscountType?>(
+              value: discountType,
+              child: Text(discountType.getTitle(appLocalizations)),
+            ),
+          ),
+        ],
+        onChanged: (DiscountType? value) {
+          if (value != null) {
+            onChanged(value);
+          }
+        },
+      ),
+    );
+  }
+}

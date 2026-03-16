a14911239854566f4f3f96c6e1d1587be9597804
elaine <elainedb.dev@gmail.com>
2026-03-16 14:34:29 +0100

    “claude”

--- packages/smooth_app/lib/background/background_task_price.dart
+++ packages/smooth_app/lib/background/background_task_price.dart
@@ -30,6 +30,7 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
     required this.pricesAreDiscounted,
     required this.prices,
     required this.pricesWithoutDiscount,
+    required this.discountTypes,
   });
 
   BackgroundTaskPrice.fromJson(super.json)
@@ -57,6 +58,12 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
       pricesWithoutDiscount = json.containsKey(_jsonTagPriceWithoutDiscount)
           ? <double?>[json[_jsonTagPriceWithoutDiscount] as double?]
           : _fromJsonListNullableDouble(json[_jsonTagPricesWithoutDiscount])!,
+      discountTypes = _normalizeDiscountTypes(
+        _fromJsonListNullableString(json[_jsonTagDiscountTypes]),
+        json.containsKey(_jsonTagBarcode)
+            ? 1
+            : (_fromJsonListString(json[_jsonTagBarcodes])?.length ?? 0),
+      ),
       super.fromJson();
 
   static const String _jsonTagDate = 'date';
@@ -71,6 +78,7 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
   static const String _jsonTagAreDiscounted = 'areDiscounted';
   static const String _jsonTagPrices = 'prices';
   static const String _jsonTagPricesWithoutDiscount = 'pricesWithoutDiscount';
+  static const String _jsonTagDiscountTypes = 'discountTypes';
   @Deprecated('Use [_jsonTagBarcodes] instead')
   static const String _jsonTagBarcode = 'barcode';
   @Deprecated('Use [_jsonTagAreDiscounted] instead')
@@ -100,6 +108,32 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
     final List<double?> result = <double?>[];
     for (final dynamic item in input) {
       result.add(item as double?);
+    }
+    return result;
+  }
+
+  static List<String> _normalizeDiscountTypes(
+    final List<String?>? input,
+    final int expectedLength,
+  ) {
+    final List<String> result = List<String>.filled(expectedLength, '');
+    if (input != null) {
+      for (int i = 0; i < input.length && i < expectedLength; i++) {
+        result[i] = input[i] ?? '';
+      }
+    }
+    return result;
+  }
+
+  static List<String?>? _fromJsonListNullableString(
+    final List<dynamic>? input,
+  ) {
+    if (input == null) {
+      return null;
+    }
+    final List<String?> result = <String?>[];
+    for (final dynamic item in input) {
+      result.add(item as String?);
     }
     return result;
   }
@@ -157,6 +191,7 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
   final List<bool> pricesAreDiscounted;
   final List<double> prices;
   final List<double?> pricesWithoutDiscount;
+  final List<String> discountTypes;
 
   @override
   Map<String, dynamic> toJson() {
@@ -173,6 +208,7 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
     result[_jsonTagAreDiscounted] = pricesAreDiscounted;
     result[_jsonTagPrices] = prices;
     result[_jsonTagPricesWithoutDiscount] = pricesWithoutDiscount;
+    result[_jsonTagDiscountTypes] = discountTypes;
     return result;
   }
 
@@ -239,6 +275,7 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
           pricesAreDiscounted: <bool>[pricesAreDiscounted[i]],
           prices: <double>[prices[i]],
           pricesWithoutDiscount: <double?>[pricesWithoutDiscount[i]],
+          discountTypes: <String>[discountTypes[i]],
         );
       }
       return;
@@ -261,6 +298,9 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
           }
         }
       }
+      final DiscountType? discountType = priceIsDiscounted
+          ? DiscountType.fromOffTag(discountTypes[i])
+          : null;
       final Price newPrice = Price()
         ..date = date
         ..currency = currency
@@ -274,6 +314,7 @@ abstract class BackgroundTaskPrice extends BackgroundTask {
         ..pricePer = isProduct ? null : PricePer.fromOffTag(pricePers[i])
         ..type = isProduct ? PriceType.product : PriceType.category
         ..priceIsDiscounted = priceIsDiscounted
+        ..discountType = discountType
         ..price = _fixPriceDecimals(price)
         ..priceWithoutDiscount = priceWithoutDiscount == null
             ? null
